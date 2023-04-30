5799ea28fa241cfa4618db9b667adc79f79f1aa8
DeepakEuler <deepaksharmaforwork@gmail.com>
2023-04-30 12:53:27 +0530

    loading csv

--- src/main/java/io/deepakeuler/ipldashboard/data/BatchConfig.java
+++ src/main/java/io/deepakeuler/ipldashboard/data/BatchConfig.java
@@ -12,8 +12,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-@Configuration
-@EnableBatchProcessing
 public class BatchConfig {
     
 @Bean
